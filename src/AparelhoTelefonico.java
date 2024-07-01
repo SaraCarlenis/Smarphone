d61bda103b2870f219992816a7a0bdbd17dcf590
SaraCarlenis <sara.hurtado.corte@hotmail.com>
2024-07-01 19:59:10 -0300

    Comitando projeto

---
+++ src/AparelhoTelefonico.java
@@ -0,0 +1,11 @@
+public class AparelhoTelefonico {
+    public void ligar() {
+        System.err.println("Ligando");
+    }
+    public void atender() {
+        System.out.println("Atender");
+    }
+    public void iniciarCorreioVoz() {
+        System.out.println("Correio de voz");   
+    }
+}
---
+++ src/NavegadorNaInternet.java
@@ -0,0 +1,12 @@
+public class NavegadorNaInternet {
+    public void exibirPagina() {
+        System.err.println("Exibindo pagina");
+    }
+    public void adicionarNovaAba() {
+        System.out.println("Nova aba");
+    }
+    public void atualizarPagina() {
+        System.out.println("Atualizando página");
+    }
+    
+}
